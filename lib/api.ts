8cb22d7110ca939675c661f32970748271f15276
Vladislaw <kumiho2708@gmail.com>
2025-10-26 20:08:35 +0200

    Initial commit

--- lib/api.ts
+++ lib/api.ts
@@ -15,9 +15,6 @@ interface FetchNotesParams {
 
 const api = axios.create({
   baseURL: 'https://notehub-public.goit.study/api',
-  headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
-  },
 })
 
 // export const fetchNotes = async ({
